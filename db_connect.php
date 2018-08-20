774012dfba2df133260a311feb1f79139e91a8af
Niel <nico_elmer@niel.site>
2018-08-20 09:13:30 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,9 +2,9 @@
 
 /* Database connection start */
 $servername = "localhost";
-$username = "bmark";
-$password = "wkDy4#05";
-$dbname = "bmark";
+$username = "bmarkr";
+$password = "";
+$dbname = "bmarkr";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
