4fe802baddb69ec77deec168525b7b11f9df5944
Niel <nico_elmer@niel.site>
2018-08-20 10:43:44 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,13 @@
 <?php
 
 /* Change scriptURL too */
-$scriptURL = "Change to sth like: https://myweb.com/anything/bmark.php";
+$scriptURL = "Change to sth like: https://myweb.com/anything/bmark.php";    //change this
 
 /* Database connection start */
-$servername = "localhost";
-$username = "bmarkr";
-$password = "";
-$dbname = "bmarkr";
+$servername = "localhost";     
+$username = "bmarkr";          //change this
+$password = "";                //change this
+$dbname = "bmarkr";            //change this
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
